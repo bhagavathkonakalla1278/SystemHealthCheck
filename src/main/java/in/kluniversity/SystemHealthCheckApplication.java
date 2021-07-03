2530ed1fe594197e3dd45f750d0145b9743beb5e
K.V.R.BHAGAVATH <190030827@kluniversity.in>
2021-07-03 18:15:39 +0530

    First time check-in Spring boot sample project

---
+++ src/main/java/in/kluniversity/SystemHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package in.kluniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemHealthCheckApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/kluniversity/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package in.kluniversity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("V1")
+public class HealthCheckController {
+
+	@GetMapping("/echo")
+	public String getEchoMessage() {
+		return "System working fine";
+	}
+	
+}
